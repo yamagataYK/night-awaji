a489ff95aea7eac89a00e7d2b98fada4fb9d9898
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-11-18 03:03:39 +0900

    secondView部分の作成

--- night-awaji/app/page.tsx
+++ night-awaji/app/page.tsx
@@ -1,6 +1,7 @@
 import styles from "./page.module.css"
 import Link from "next/link";
 import Image from "next/image";
+import SecondView from "../public/secondView.jpeg"
 
 
 
@@ -18,9 +19,9 @@ export default function Home() {
               className={styles.experience}
             >体験する</Link >
           </header>
-          <main>
-            <h2 className={styles.first_text}>淡路島で心動く体験を。</h2>
-          </main>
+
+          <h2 className={styles.first_text}>淡路島で心動く体験を。</h2>
+
         </div>
         <section className={styles.int_Wrap}>
           <h3>「夜の淡路島を、ただ知るだけでなく体験するためのサイト。」</h3>
@@ -28,6 +29,22 @@ export default function Home() {
             あなたの夜が、淡路島で動き出します。</p>
         </section>
       </div>
+      <div className={styles.second_view}>
+        <Image
+          src={SecondView}
+          alt="景色の写真"
+          fill
+          className={styles.second_image}
+        ></Image>
+      </div>
+
+      <section className={styles.info_wrap}>
+
+        <p className={styles.text}>かかかか</p>
+
+      </section>
+
+
     </>
   );
 }
