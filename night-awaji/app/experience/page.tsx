6549806569dfaa67edd0b8e8c87d3f79420dad3a
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-12-13 17:27:58 +0900

    現在地の取得

--- night-awaji/app/experience/page.tsx
+++ night-awaji/app/experience/page.tsx
@@ -1,27 +1,43 @@
 "use client";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+type LatLng = [number, number];
 
 export default function Experience() {
-    const position = [34.6937, 135.5023];
+    const [pos, setPos] = useState<LatLng | null>(null);
+
+    const myIcon = L.icon({
+        iconUrl: "/myIcon.png",
+        iconSize: [30, 30],
+        iconAnchor: [20, 40],
+    });
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((p) => {
+            setPos([p.coords.latitude, p.coords.longitude]);
+        });
+    }, []);
+
+    const center: LatLng = pos ?? [34.6937, 135.5023];
+
 
     return (
         <>
             <MapContainer
-                center={position}
+                center={center}
                 zoom={13}
                 scrollWheelZoom={false}
-                style={{ height: "100vh", width: "100%" }}>
+                style={{ height: "100vh", width: "100%" }}
+            >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
+                {pos && <Marker position={pos} icon={myIcon} />}
+            </MapContainer >
         </>
     )
 }
